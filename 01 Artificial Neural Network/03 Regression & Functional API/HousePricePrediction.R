1451b521d647f3eace5e78ce9af5240a37a9831f
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-03-06 11:35:21 +0530

    [ADD] Callbacks, Saving Models & Best Models and Functional API

--- 01 Artificial Neural Network/03 Regression & Functional API/HousePricePrediction.R
+++ 01 Artificial Neural Network/03 Regression & Functional API/HousePricePrediction.R
@@ -39,3 +39,78 @@ model %>% fit(train_data, train_labels, epoch=30, batch_size=100)
 # Test Performance
 score <- model %>% evaluate(test_data, test_labels)
 score
+
+##### Saving and Restoring Models #####
+
+model %>% save_model_hdf5("my_model.h5")
+
+new_model <- load_model_hdf5("my_model.h5")
+
+model %>% summary()
+# or you can write summary(model)
+new_model %>% summary()
+
+checkpoint_dir <- "checkpoints"
+
+dir.create(checkpoint_dir, showWarnings = FALSE)
+
+filepath <- file.path(checkpoint_dir, "Epoch-{epoch:02d}.hdf5")
+
+# Create checkpoint callback
+cp_callback <- callback_model_checkpoint(filepath = filepath)
+
+rm(model)
+k_clear_session()
+
+model_callback <- keras_model(inputs = inputs, outputs = predictions)
+model_callback %>% compile(optimizer = 'rmsprop',loss = 'mse',
+                           metrics = list("mean_absolute_error"))
+
+model_callback %>% fit(train_data, train_labels, epochs = 30,
+                       callbacks = list(cp_callback))
+
+list.files(checkpoint_dir)
+
+tenth_model <- load_model_hdf5(file.path(checkpoint_dir, "Epoch-10.hdf5"))
+
+summary(tenth_model)
+
+##### Only saving the best model
+
+callbacks_best <- callback_model_checkpoint(filepath = "best_model.h5", monitor = "val_loss", 
+                                            save_best_only = TRUE)
+
+rm(model_callback)
+k_clear_session()
+
+model_cb_best <- keras_model(inputs = inputs, outputs = predictions)
+model_cb_best %>% compile(optimizer = 'rmsprop',loss = 'mse',
+                          metrics = list("mean_absolute_error"))
+
+model_cb_best %>% fit(train_data, train_labels, epochs = 30, 
+                      validation_data=list(test_data,test_labels),
+                      callbacks = list(callbacks_best))
+
+best_model <- load_model_hdf5("best_model.h5")
+
+### Stopping the processing when we find the best model
+
+callbacks_list <- list(
+  callback_early_stopping(monitor = "val_loss",patience = 3),
+  callback_model_checkpoint(filepath = "best_model_early_stopping.h5", monitor = "val_loss", save_best_only = TRUE)
+)
+
+rm(model_cb_best)
+k_clear_session()
+
+model_cb_early <- keras_model(inputs = inputs, outputs = predictions)
+model_cb_early %>% compile(optimizer = 'rmsprop',loss = 'mse',
+                           metrics = list("mean_absolute_error"))
+
+model_cb_early %>% fit(train_data, train_labels, epochs = 100, 
+                       validation_data=list(test_data,test_labels),
+                       callbacks = callbacks_list)
+
+best_model_early_stopping <- load_model_hdf5("best_model_early_stopping.h5")
+
+k_clear_session()
